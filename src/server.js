015864b783b7c3ed5834b92acd9b657705203cdf
Ngo Hieu Phat <ngohipha@gmail.com>
2022-12-08 20:01:24 +0700

    cau hinh thu muc

---
+++ src/server.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+import  express  from 'express';
+import configViewEngine  from './configs/configviewengine';
+const app = express()
+const port = 3000
+configViewEngine(app);
+app.get('/', (req, res) => {
+  res.render("index.ejs");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
