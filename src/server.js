3337782667441547eacf45676d51b2f2ceaaab69
Ngo Hieu Phat <ngohipha@gmail.com>
2022-12-19 19:45:14 +0700

    morgan,middleware

--- src/server.js
+++ src/server.js
@@ -5,8 +5,18 @@ import configViewEngine  from './configs/configviewengine';
 import initAPIRoute from './route/api';
 import connection from './configs/connectDB';
 require('dotenv').config();
+var morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 8080;
+
+app.use(morgan('combined'))
+app.use((req,res, next)=>{
+  // check => return res.send
+  console.log('run into middleware');
+  console.log(req.method);
+  next();
+});
+// chuyen du lieu sang json //middleware to parse JSON data from request
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 // setup view engine
@@ -15,6 +25,10 @@ configViewEngine(app);
 initwedRoute(app);
 // init api route
 initAPIRoute(app);
+// handle 404 not found
+app.use((req,res)=>{
+  return res.render('404.ejs')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
